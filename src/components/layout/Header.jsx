2c8935a6ee7f70d23a472b4cdf1f07f58ed79db7
Amir <amir.hamishegi2@gmail.com>
2026-07-13 14:17:26 +0330

    make custom breck point and finish top Header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -12,11 +12,11 @@ const Header = () => {
     return (
         <div className='bg-white w-full'>
             <div className='bg-[#caddff]'>
-                 <div className='w-[85%] lg:w-[90%] mx-auto'>
-                    <div className='flex justify-between items-center  h-12.5'>
+                 <div className='w-[85%] lg:w-[90%] mx-auto max-md-lg-custom:hidden'>
+                    <div className='flex justify-between items-center text-slate-500  h-12.5'>
                         {/* Email & Phone Number */}
-                    <ul className='flex justify-center items-center gap-3 text-black'>
-                        <li className='flex justify-start items-center gap-3 after:h-4.5 after:w-px after:bg-slate-500 font-normal'>
+                    <ul className='flex justify-start items-center gap-3 text-black font-semibold'>
+                        <li className='flex justify-start items-center gap-3 after:h-4.5 after:w-px after:bg-[#afafaf]  text-sm'>
                             <span><MdEmail/></span>
                             <span>Amir@gmail.com</span>
                         </li>
@@ -25,10 +25,10 @@ const Header = () => {
                             <span>+989371414035</span>
                         </li>
                     </ul>
-                   <div className='flex justify-center items-center gap-7'>
+                   <div className='flex justify-center items-center gap-7 '>
                     {/* Social Media */}
 
-                     <div className='flex justify-start items-center gap-4.5'>
+                     <div className='flex justify-start text-black items-center gap-4.5'>
                     <Link className='text-lg'>
                         <FaFacebook/>
                     </Link>
@@ -43,19 +43,26 @@ const Header = () => {
                     </Link>
                     </div>
                     {/* Language */}
-                    <div className='flex justify-center items-center'>
-                        <span className='flex gap-6 justify-center items-center after:h-4.5 after:w-px after:bg-slate-500
+                    <div className='flex justify-center items-center group relative'>
+                        <span className=' flex gap-6 justify-center items-center after:h-4.5 after:w-px after:bg-slate-500
                         before:h-4.5 before:w-px before:bg-slate-500 cursor-pointer'>
                             <img src="/frontend/images/language.png" alt="" />
                         </span>
+
+                        {/* Hover Box Show */}
+                        <div className='bg-black transition-all invisible group-hover:visible text-white absolute top-7 left-0 w-25 justify-center items-center flex flex-col   rounded-md'>
+                            <span className='hover:bg-red-500 inline-block w-full py-1 text-center hover:text-black cursor-pointer'>English</span>
+                            <span className='hover:bg-red-500 inline-block w-full py-1 text-center hover:text-black cursor-pointer'>Persian</span>
+
+                        </div>
                     </div>
                         {/* User Login & Info */}
                         <Link>
                             {
-                                user ? <div className='flex justify-center items-center gap-2'>
+                                user ? <div className='text-black flex justify-center items-center gap-2'>
                                     <span><FaUser/></span>
                                     <span className='font-bold'>Amir</span>
-                                </div> : <div className='flex justify-center items-center gap-2'>
+                                </div> : <div className='flex text-black justify-center items-center gap-2'>
                                     <span><FaUserLock/></span>
                                     <span className='font-semibold'>Login</span>
                                 </div>
