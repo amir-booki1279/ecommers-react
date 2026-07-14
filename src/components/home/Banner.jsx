a3f96acf35627bd9bcd9c742b1696d4623bd3186
Amir <amir.hamishegi2@gmail.com>
2026-07-14 14:16:55 +0330

    finish Banner Section

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,9 +1,55 @@
 import  'react';
+import Carousel from 'react-multi-carousel';
+import { Link } from 'react-router-dom';
+import 'react-multi-carousel/lib/styles.css'
 
+const Slider = Carousel?.default || Carousel;
 const Banner = () => {
+
+    const responsive = {
+        superLargeDesktop : {
+            breakpoint : {max:4000,min:3000},
+            items:1
+        },
+        desktop : {
+            breakpoint : {max:3000,min:102},
+            items:1
+        },
+        tablet : {
+            breakpoint : {max:1024,min:464},
+            items:1
+        },
+        mobile : {
+            breakpoint : {max:464,min:0},
+            items:1
+        },
+    }
+
     return (
-        <div>
-            
+        <div className='w-full max-md-lg-custom:mt-6'>
+            <div className='w-[85%] max-xl:w-[90%] mx-auto'>
+                <div className='w-full flex flex-wrap max-md-lg-custom:gap-8'>
+                    <div className='w-full'>
+                            <div className='my-8'>
+                                <Slider
+                                  autoPlay={true}
+                                  infinite = {true}
+                                  arrows={true}
+                                  showDots={true}
+                                  responsive={responsive}
+                                >
+                                    {
+                                        [1,2,3,4,5,6].map((img,i)=>(
+                                            <Link key={i}>
+                                                <img src={`/frontend/images/banner/${img}.jpg`} alt="" />
+                                            </Link>
+                                        ))
+                                    }
+                                </Slider>
+                            </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
